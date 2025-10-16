ad3445b09f3224fc7c5879c57a5fc5dc2e6ad2b7
jwill7022 <jwill702@charlotte.edu>
2025-10-16 16:00:55 -0400

    feat: stuff and things

--- Labs/Lab5/lab6-starter/js/script.js
+++ Labs/Lab5/lab6-starter/js/script.js
@@ -37,9 +37,101 @@ articles.forEach(article=>{
  * @param {object} article - an article
  */
 function addEntry(article) {
+  
+    //create a new article
+    const newEntry = document.createElement('article');
+    newEntry.setAttribute('class', 'article-container');
+
+    
+    //Add new article to list of articles
+    const articlesWrapper = document.querySelector('.articles-wrapper');
+    articlesWrapper.appendChild(newEntry);
+
+    //Delete Button
+    const button = document.createElement('button');
+    button.textContent = 'x';
+    button.setAttribute('class', 'delete-btn');
+    newEntry.appendChild(button);
+
+  //Header
+  const headerDiv = document.createElement('div');
+  headerDiv.setAttribute('class', 'article-header');
+  
+  const avatar = document.createElement('img');
+  avatar.setAttribute('class', 'avatar');
+
+  const authorIndex = authors.indexOf(article.author);
+  if(authorIndex != -1){
+    avatar.setAttribute('src', `images/avatar${authorIndex}.png`);
+  }
+  else{
+    avatar.setAttribute('src', 'images/default.jpeg');
+  }
+
+  avatar.setAttribute('alt', 'avatar picture');
+
+  const authorDateDiv = document.createElement('div');
+  authorDateDiv.setAttribute('class', 'author-date');
+  authorDateDiv.textContent = `${article.author} · ${article.date.toDateString()}`;
+
+  headerDiv.appendChild(avatar);
+  headerDiv.appendChild(authorDateDiv);
+  newEntry.appendChild(headerDiv);
+
+  //Body
+  const body = document.createElement('div');
+  body.setAttribute('class', 'article-body');
+  const title = document.createElement('h3');
+  title.textContent = article.title;
+  const content = document.createElement('p');
+
+  if(article.content.length < MAX_LENGTH){
+    body.appendChild(title);
+    content.textContent = article.content.substring(0, MAX_LENGTH);
+    body.appendChild(content);
+  }
+  else{
+    body.appendChild(title);
+    splitContent(article, body, content);
+  }
+
+  
+  newEntry.appendChild(body);
 
 }
 
+    //content split into two paragraphs if max length exceeded
+function splitContent(article, body, content){
+  content.textContent = article.content.slice(0, MAX_LENGTH);
+  body.append(content);
+  const elipse = document.createElement('span');
+  elipse.textContent = '...';
+  content.append(elipse);
+  const hiddenText = document.createElement('span');
+  hiddenText.setAttribute('class', 'hidden');
+  hiddenText.textContent = article.content.slice(MAX_LENGTH + 1, article.content.length);
+  content.append(hiddenText);
+  const readBtn = document.createElement('button');
+  readBtn.textContent = "Read More";
+  readBtn.setAttribute('class', 'btn');
+  body.append(readBtn);
+}
 
+// adding event listener to respond to form submission
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event){
+
+  if(form.reportValidity()){
+    preventDefault();
+    const title = document.getElementById('title').value = '';
+    const author = document.getElementById('author').value = '';
+    const content = form.querySelector('#content').value = '';
+    const date = new Date();
+    articles.push({title, author, date, content});
+    addEntry(article);
+    form.reset();
+  }
+});
 
 
